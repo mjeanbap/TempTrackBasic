c35be14dcb5a3285b6cd82dbf51757dc170ba3ef
Marvrick Jean-Baptiste <mjeanbap@users.noreply.github.com>
2018-06-04 17:48:20 -0400

    Add files via upload

--- TempTrack.ino
+++ TempTrack.ino
@@ -1,30 +1,11 @@
 #include <Adafruit_SSD1306.h>
-
-/*********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-This example is for a 128x64 size display using I2C to communicate
-3 pins are required to interface (2 I2C and one reset)
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-*********************************************************************/
-
-
-
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
+#define LED 13
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
@@ -35,7 +16,14 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 
 #define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
+#define LOGO16_GLCD_WIDTH  16 0
+
+
+#define DHTPIN 2     // what digital pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE);
+
+
 static unsigned char PROGMEM const logo16_glcd_bmp[] =
 { B00000000, B11000000,
   B00000001, B11000000,
@@ -60,41 +48,12 @@ static unsigned char PROGMEM const logo16_glcd_bmp[] =
 
 
 
-  #include <DHT.h>
-#include <DHT_U.h>
-
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
-
-#define DHTPIN 2     // what digital pin we're connected to
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
-DHT dht(DHTPIN, DHTTYPE);
-
-#define LED 13
-
 void setup()   {   
   Serial.begin(9600);
   Serial.println("DHTxx test!");
   dht.begin();        
   
   pinMode(LED, OUTPUT);     
-/**********************************************************/
 
   delay(200);
 
@@ -105,97 +64,10 @@ void setup()   {
   display.display(); // show splashscreen
   delay(2000);
   display.clearDisplay();   // clears the screen and buffer
-
-  
-
-  /*// draw a single pixel
-  display.drawPixel(10, 10, WHITE);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw many lines
-  testdrawline();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw rectangles
-  testdrawrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw multiple rectangles
-  testfillrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw mulitple circles
-  testdrawcircle();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw a white circle, 10 pixel radius
-  display.fillCircle(display.width()/2, display.height()/2, 10, WHITE);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testfillroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawtriangle();
-  delay(2000);
-  display.clearDisplay();
-   
-  testfilltriangle();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw the first ~12 characters in the font
-  testdrawchar();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw scrolling text
-  testscrolltext();
-  delay(2000);
-  display.clearDisplay();
-*/
-
-
-/*
-  // miniature bitmap display
-  display.clearDisplay();
-  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-  display.display();
-
-  // invert the display
-  display.invertDisplay(true);
-  delay(1000); 
-  display.invertDisplay(false);
-  delay(1000); 
-
-  // draw a bitmap icon and 'animate' movement
-  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
-
-*/
-
 }
 
 
 void loop() {
-
-
 
   // Wait a few seconds between measurements.
   delay(2000);
@@ -219,30 +91,12 @@ void loop() {
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
-
-
-
-  /**********************************************************/
   
   display.clearDisplay(); 
   // text display tests
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  
   display.print("Temperature is: ");display.print(f);
   display.print("Humidity is: "); display.println(h);
   if (f < 75)
@@ -259,22 +113,5 @@ void loop() {
   delay(2000);
   display.clearDisplay(); 
 
-  if (f < 70)
-    {
-        digitalWrite(LED, HIGH);
-      }
-      else
-      {
-         digitalWrite(LED, LOW);
-        }
-  /*display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0,8);
-  display.println("MAVTH3KID");
-  display.display();
-  
-  delay(2000);
-
-  display.clearDisplay();
- */
 }
+
